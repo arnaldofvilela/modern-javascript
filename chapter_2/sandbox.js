4025678fa894335911a3c515655c8dd12d4578a6
Arnaldo Neto <vilela-neto@hotmail.com>
2021-08-28 02:03:18 -0300

    até aula 7

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,3 +1,15 @@
-// alert("hello, world");
-console.log(1);
-console.log(2);
+let age = 25;
+let year = 2019;
+
+// log things to console
+console.log(age, year);
+
+age = 30;
+
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);
