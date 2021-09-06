8195b6b5274d538fd2842b1bae78a90d418728e1
Arnaldo Neto <vilela-neto@hotmail.com>
2021-09-06 11:17:42 -0300

    aula 23

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,19 +1,11 @@
-// // if statements
-// const age = 25;
+// // else if statements
 
-// if(age > 20){
-//     console.log('you are over 20 years old');
-// }
+const password = 'p@sswo';
 
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if(ninjas.length > 4){
-//     console.log("that's a lot of ninjas");
-// }
-
-
-const password = 'p@ssword';
-
-if(password.length >= 8){
+if (password.length >= 12){
+    console.log('that password is mighty strong');
+} else if(password.length >= 8){
     console.log('that password is long enough!');
+} else {
+    console.log('password is not long enough');
 }
