a73ed12541a60f90b69a794ccc04218babca0fab
Arnaldo Neto <vilela-neto@hotmail.com>
2021-09-06 23:25:03 -0300

    aula 30

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,16 +1,21 @@
-// variables & block scope
-const age = 30;
 
-if(true){
-    const age = 40;
-    const name = 'shaun';
-    console.log('inside 1st code block: ', age, name);    
 
-    if(true){
-        const age = 50;
-        console.log('inside 2nd code blok: ', age);
-    }
 
+
+// greet();
+// greet();
+// greet();
+
+speak();
+speak();
+speak();
+
+// function declaration
+function greet(){
+    console.log('hello there');
 }
 
-console.log('outside code block: ', age, name);
+// function expression
+const speak = function(){
+    console.log('good day!');
+};
