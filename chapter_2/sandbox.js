f0f36ec5d2b1cbcfb99b1a79a009b41018eeec63
Arnaldo Neto <vilela-neto@hotmail.com>
2021-09-07 07:39:19 -0300

    aula 32

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,8 +1,19 @@
-// arguments & parameters
+// returning values
 
-const speak = function(name = 'luigi', time = 'night'){
-    console.log(`good ${time} ${name}`);
+// const speak = function(name = 'luigi', time = 'night'){
+//     console.log(`good ${time} ${name}`);
+// };
+
+const calcArea = function(radius){
+    return 3.14 * radius**2;
+    
+}
+
+const area = calcArea(5);
+console.log(area);
+
+const calcVol = function(area){
+
 };
 
-speak(); 
-speak('shaun'); 
+calcVol(area);
