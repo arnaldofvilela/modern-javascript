e5237f63a2d4ed3c28bd61cee1abcc60c9adaa1f
Arnaldo Neto <vilela-neto@hotmail.com>
2021-09-07 08:44:54 -0300

    aula 34

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,58 +1,12 @@
-// regular function
-// const calcArea = function(radius){
-//     return 3.14 * radius**2;
-// }
+const name = 'shaun';
 
+// functions
 
-// arrow function 
+const greet = () => 'hello';
 
-// const calcArea = radius => 3.14 * radius ** 2;
+let resultOne = greet();
+console.log(resultOne);
 
-
-
-// const area = calcArea(5);
-// console.log('area is: ', area);
-
-
-
-
-
-
-
-// practise arrow functions
-
-// const greet = function(){
-//     return 'hello, world';
-// }
-
-// const greet = () => 'hello world';
-
-// const result = greet();
-// console.log(result);
-
-// const bill = function(products, tax){
-//     let total = 0;
-//     for(let i = 0; i < products.length; i++){
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total;
-// }
-
-const bill = (products, tax) => {
-    let total = 0;
-    for(let i = 0; i < products.length; i++){
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-};
-
-console.log(bill([10,15,30], 0.2));
-
-
-// const bill = (products, tax) => {
-//     let total = 0;
-//     for(let i = 0; i < products.length; i++){
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total;
-// }
+// methods
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
