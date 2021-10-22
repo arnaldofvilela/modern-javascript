531f3a9f09755e6baffdfb759970149164a7a4a2
Arnaldo Neto <vilela-neto@hotmail.com>
2021-10-22 18:29:01 -0300

    Lesson 55. Event Bubbling (and delegation)

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,25 +1,26 @@
 const ul = document.querySelector('ul');
-// ul.remove();
-
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    // ul.innerHTML += '<li>something new</li>';
     const li = document.createElement('li');
     li.textContent = 'something new to do';
-    // ul.append(li);
     ul.prepend(li);
 });
 
-const items = document.querySelectorAll('li');
+// const items = document.querySelectorAll('li');
 
-items.forEach(item => {
-    item.addEventListener('click', e => {
-        // console.log('item clicked');
-        // console.log(e);
-        // console.log(e.target);
-        // console.log(item);
-        // e.target.style.textDecoration = 'line-through';
+// items.forEach(item => {
+//     item.addEventListener('click', e => {
+//         console.log('event in LI');
+//         e.stopPropagation();
+//         e.target.remove();
+//     });
+// });
+
+ul.addEventListener('click', e => {
+    // console.log('event in UL');
+    // console.log(e);
+    if(e.target.tagName === 'LI'){
         e.target.remove();
-    })
+    }
 })
