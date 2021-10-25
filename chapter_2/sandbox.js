85a43b22912d04721519052adea4a999f22f0050
Arnaldo Neto <vilela-neto@hotmail.com>
2021-10-25 18:59:04 -0300

    Lesson 61. Testing RegEx Patterns

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -6,3 +6,20 @@ form.addEventListener('submit', e => {
     // console.log(username.value);
     console.log(form.username.value);
 });
+
+// testing RegEx
+const username = 'shaun';
+const pattern = /^[a-z]{6,}$/;
+
+// let result = pattern.test(username);
+
+// console.log(result);
+
+// if(result){
+//     console.log('regex test passed :)');
+// } else {
+//     console.log('regex test failed :( ');
+// }
+
+let result = username.search(pattern);
+console.log(result);
