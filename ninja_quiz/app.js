887b949b02a4c0e49b13db8768f5db54362423aa
Arnaldo Neto <vilela-neto@hotmail.com>
2021-10-31 02:24:49 -0300

    Lesson 70. Intervals & Animating the Score

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -17,9 +17,21 @@ form.addEventListener('submit', e => {
 
     // show result on page
     scrollTo(0,0);
-    result.querySelector('span').textContent = `${score}%`;
+    // result.querySelector('span').textContent = `${score}%`;
     result.classList.remove('d-none');
-    console.log(score);
+    
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++;        
+        }
+    }, 10);
+
+
 
 });
 
@@ -39,3 +51,13 @@ form.addEventListener('submit', e => {
 // setTimeout(() => {
 //     alert('hello, ninjas');
 // }, 3000);
+
+// let i = 0;
+// const timer = setInterval(() => {
+//     console.log('hello');
+//     i++;
+//     if(i === 5){
+//         clearInterval(timer);
+//     }
+// }, 1000);
+
